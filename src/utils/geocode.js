6fd574d43bcd93d31fbdf82ac60043bfcc1af042
Sergey <job4sergey@gmail.com>
2020-10-12 19:09:11 +0700

    initial

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoic3NldmVyb3YiLCJhIjoiY2tnNjlyaDE3MGl6aTJ5cjB0NDduemYwaiJ9.X4B1QcJv-Vntl5Wi3hII5g&limit=1'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
